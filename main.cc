5eec326afce537a1e1e7d614872392ec4ef12beb
r67zhao <r67zhao@uwaterloo.ca>
2019-11-24 23:36:11 -0500

    implement main.cc

--- main.cc
+++ main.cc
@@ -1,5 +1,52 @@
 #include "game.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
-int main() {
+int main()
+{
+    Game game;
 
+    string cmd;
+
+    while (cin >> cmd)
+    {
+        if (cmd == "move")
+        {
+            char link;
+            cin >> link;
+            Direction dir;
+            string dirtemp;
+            cin >> dirtemp;
+            if (dirtemp == "left") dir = Direction::Left;
+            if (dirtemp == "right") dir = Direction::Right;
+            if (dirtemp == "up") dir = Direction::Up;
+            else dir = Direction::Down;   // note the absence of error checking
+            game.move(link, dir);
+        }
+        else if (cmd == "abilities")
+        {
+            // ITERATION II
+        }
+        else if (cmd == "ability")
+        {
+            // ITERATION II
+        }
+        else if (cmd == "board")
+        {
+            // game.print();
+        }
+        else if (cmd == "sequence")
+        {
+            // TODO, can add a stream to the front of another?
+        }
+        else if (cmd == "quit")
+        {
+            break;
+        }
+        else
+        { // invalid command, take input again
+            cout << "invalid command, try again" << endl;
+        }
+    }
 }
