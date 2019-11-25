9106855769ebae320e8833cc6b117d1d4a32eae9
vjsun <victor.jianzhou.sun@edu.uwaterloo.ca>
2019-11-24 19:36:25 -0500

    Add link implementation

--- link.cc
+++ link.cc
@@ -1,1 +1,24 @@
 #include "link.h"
+
+Link::Link(LinkType type, int strength, char name): 
+    type{type}, strength{strength}, name{name} {}
+
+LinkType Link::getType() {
+    return type;
+}
+
+void Link::setType(LinkType newType) {
+    type = newType;
+}
+
+int Link::getStrength() {
+    return strength;
+}
+
+void Link::setStrength(int newStrength) {
+    strength = newStrength;
+}
+
+char Link::getName() {
+    return name;
+}
