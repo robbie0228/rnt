5253a1ab5f64b834ae329fd286453cb1ad50fb9c
iskim <iskim@student.cs.uwaterloo.ca>
2019-11-24 22:50:14 -0500

    Adding int field currentPlayer to Game

--- game.h
+++ game.h
@@ -1,10 +1,12 @@
 #include <vector>
+#include <iostream>
 #include "grid.h"
 #include "player.h"
 
 class Game {
         Grid grid;     // grid is the Grid that the game takes place in
         std::vector<Player> players;    // a vector of two Player's
+        int currentPlayer; // an integer that holds info on the current Player
 
     public:
         // Constructor for a Game
@@ -15,10 +17,11 @@ class Game {
         // Additional construction may go here if necessary
         void init();
 
-        // Calls Grid's print()
-        void print();
-
         // Moves Link-link towards Direction dir;
         //   Parses link and calls Grid's move() with parsed information
         void move(char link, Direction dir);
+
+        friend std::ostream &operator<<(std::ostream &out, const Game &g);
 };
+
+
