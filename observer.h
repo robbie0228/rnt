b5a28b0acd2bdbb1fc9d1730c1a39c40a8e1c0bb
Victor Sun <victor.jianzhou.sun@uwaterloo.ca>
2019-11-28 15:20:50 -0500

    Update observer.h formatting

--- observer.h
+++ observer.h
@@ -1,11 +1,12 @@
-#ifndef OBSERVER_H_
-#define OBSERVER_H_
+#ifndef OBSERVER_H
+#define OBSERVER_H
 
 class Subject;
 
 class Observer {
-  public:
-  virtual void notify(Subject &whoFrom) = 0; // pass the Subject that called the notify method
-  virtual ~Observer() = default;
+    public:
+        virtual void notify(Subject &whoFrom) = 0; // pass the Subject that called the notify method
+        virtual ~Observer() = default;
 };
+
 #endif
