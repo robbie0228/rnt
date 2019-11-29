cafb668af4d329c7c4c94015106f22192ba0c34f
r67zhao <r67zhao@uwaterloo.ca>
2019-11-28 22:53:45 -0500

    complete text_display.h

--- text_display.h
+++ text_display.h
@@ -9,10 +9,19 @@ using std::vector;
 using std::ostream;
 
 class TextDisplay {
-        vector<vector<char>> grid;
+        // 8*8 2D array of (. a-h A-H S m w)
+        vector<vector<char>> grid; 
+        
+        // 2 player's 8 of pair(a-hA-H, D/V1-8)
+        //   note that steal 2 to have 4 steroids makes a max strength of 8
         vector<vector<std::pair<char, std::string>>> links;
+
+        // 2 player's 8 of the other player's links' reveal status
+        //   true means revealed to this player
         vector<vector<bool>> knownLinks;
+        
     public:
+        TextDisplay();
         void notify(Subject&);
         friend ostream& operator<< (ostream&, TextDisplay&);
 };
