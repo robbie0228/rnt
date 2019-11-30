dc31be02f1e22ec8da0bca4d6639a223832edf90
iskim <iskim@student.cs.uwaterloo.ca>
2019-11-30 17:27:00 -0500

    Resolving bug after merging 28

--- game.cc
+++ game.cc
@@ -68,5 +68,5 @@ pair<int, bool> Game::verifyAbility(int abilityID) {
 
 void Game::useAbility(int abilityID, vector<char> useAbilityInfo) {
     Ability abilityName = players[currentPlayer].useAbility(abilityID, useAbilityInfo);
-    grid.useAbility(abilityName, useAbilityInfo);
+    grid->useAbility(abilityName, useAbilityInfo);
 }
