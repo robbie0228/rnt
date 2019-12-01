c5a1d29c8d4b667b4b0442cee890c4b6f686ee61
vjsun <victor.jianzhou.sun@edu.uwaterloo.ca>
2019-12-01 17:41:18 -0500

    Add graphics

---
+++ graphicsDisplay.cc
@@ -0,0 +1,165 @@
+#include "graphicsDisplay.h"
+
+GraphicsDisplay::GraphicsDisplay(size_t n) : gridSize{n} {
+	this->grid = grid;
+	this->links = links;
+
+	knownLinks = vector<vector<bool>>(NUMPLAYERS,
+									  vector<bool>(NUMLINKS, false));
+
+	downloadedCounts = vector<pair<int, int>>(NUMPLAYERS,
+											  pair<int, int>(0, 0));
+
+	abilityRemainingCounts = vector<int>(NUMPLAYERS, NUMABILITIES);
+
+	// Create window
+	win = Xwindow(500, 1000);
+
+	// Create blue background
+	win->fillRectangle(0, 0, 500, 1000, 4);
+
+	// Create dividing lines
+	size_t pieceSize = 500 / GRIDSIZE;
+	size_t lineWidth = 2;
+	for (size_t i = 1; i < n; ++i)
+	{
+		win->fillRectangle((i * pieceSize) - (lineWidth / 2), 250, lineWidth, 500, 3);
+	}
+	for (size_t i = 1; i < n; ++i)
+	{
+		win->fillRectangle(0, 250 + (i * pieceSize) - (lineWidth / 2), 500, lineWidth, 3);
+	}
+}
+
+void TextDisplay::notify(Subject &whoFrom) {
+    StateType state = whoFrom.getState();
+    InfoType info = whoFrom.getInfo();
+
+    int linkDownloader = state.downloadingPlayer - 1;
+    // When a player is downloading a link
+    if (linkDownloader != -2) {
+        if (state.downloadingLinkType == LinkType::Data) {
+            downloadedCounts[linkDownloader].first += 1;
+        } else {
+            downloadedCounts[linkDownloader].second += 1;
+        }
+
+        // When two links battle and the downloading link is revealed
+        if (state.downloadingLinkIsRevealed) {
+            if (state.downloadingLinkName - 'a' >= 0) {
+                knownLinks[linkDownloader][state.downloadingLinkName - 'a'] = true;
+            } else {
+                knownLinks[linkDownloader][state.downloadingLinkName - 'A'] = true;
+            }
+        }
+    }
+
+    int abilityUser = state.playerUsingAbility - 1;
+    // When a player is using an ability
+    if (state.playerUsingAbility != -2) {
+        abilityRemainingCounts[abilityUser] -= 1;
+    }
+
+    grid[info.row][info.col] = info.name;
+
+    // When the cell contains a link
+    if ((0 <= info.name - 'a' && info.name - 'a' < NUMLINKS)
+        || (0 <= info.name - 'A' && info.name - 'A' < NUMLINKS)) {
+        // When a link is revealed using reveal
+        if (state.linkIsRevealed) {
+            if (info.name - 'a' >= 0) {
+                knownLinks[1][info.name - 'a'] = true;
+            } else {
+                knownLinks[0][info.name - 'A'] = true;
+            }
+        }
+        
+        string linkTypeAndStrength = 
+                    (info.type == LinkType::Data ? "D" : "V") + 
+                    to_string(info.strength);
+        if (info.name - 'a' >= 0) {
+            links[0][info.name - 'a'] = 
+                make_pair(info.name, linkTypeAndStrength);
+        } else {
+            links[1][info.name - 'A'] = 
+                make_pair(info.name, linkTypeAndStrength);
+        }
+    }
+}
+
+void GraphicsDisplay::draw(int currentPlayer) {
+    string s = "Player 1:" + "\n"
+        + "Downloaded: " + downloadedCounts[0].first + "D, " 
+                          + downloadedCounts[0].second + "V" + "\n"
+        + "Abilities: " + abilityRemainingCounts[0] + "\n";
+
+    if (currentPlayer == 0) {
+        for (int i = 0; i < 8; ++i) {
+            s += links[0][i].first + ": " 
+                + links[0][i].second;
+            if (i == 3 || i == 7) s += "\n";
+            else s += "   ";
+        }
+    }
+    else if (currentPlayer == 1) {
+        for (int i = 0; i < 8; ++i) {
+            s += links[0][i].first + ": ";
+            if (knownLinks[1][i]) {
+                s += links[0][i].second;
+                if (i == 3 || i == 7) s += "\n";
+                else s += "   ";
+            } else {
+                s += "?";
+                if (i == 3 || i == 7) s += "\n";
+                else s += "    ";
+            }
+        }
+    }
+    else {
+        throw "this player does not exist";
+    }
+
+    s += string(8, '=') + "\n";
+	win.drawString(0,0,s)
+
+    for (int row = 0; row < GRIDSIZE; ++row) {
+        for (int col = 0; col < GRIDSIZE; ++col) {
+            s = grid[row][col];
+        }
+        s = "\n";
+    }
+
+    s = string(8, '=') + "\n";
+
+     s += "Player 2:" + "\n"
+        + "Downloaded: " + downloadedCounts[1].first + "D, " 
+                          + downloadedCounts[1].second + "V" + "\n"
+        + "Abilities: " + abilityRemainingCounts[1] + "\n";
+
+    if (currentPlayer == 0) {
+        for (int i = 0; i < 8; ++i) {
+            s += links[1][i].first + ": ";
+            if (knownLinks[0][i]) {
+                s += links[1][i].second;
+                if (i == 3 || i == 7) s += "\n";
+                else s += "   ";
+            } else {
+                s += "?";
+                if (i == 3 || i == 7) s += "\n";
+                else s += "    ";
+            }
+        }
+    }
+    else if (currentPlayer == 1) {
+        for (int i = 0; i < 8; ++i) {
+            s += links[1][i].first + ": " 
+                + links[1][i].second;
+            if (i == 3 || i == 7) s += "\n";
+            else s += "   ";
+        }
+    }
+    else {
+        throw "this player does not exist";
+    }
+	win.drawString(0,750,s)
+}
---
+++ graphicsDisplay.h
@@ -0,0 +1,39 @@
+#ifndef GRAPHICSDISPLAY_H
+#define GRAPHICSDISPLAY_H
+#include <iostream>
+#include <vector>
+#include "subject.h"
+#include "enums.h"
+#include "window.h"
+
+class GraphicsDisplay: public Observer {
+        Xwindow *win; // reference to Xwindow
+        // 8*8 2D array of (. a-h A-H S m w)
+        vector<vector<char>> grid; 
+        
+        // 2 player's 8 of pair(a-hA-H, D/V1-8)
+        //   note that steal 2 to have 4 steroids makes a max strength of 8
+        vector<vector<std::pair<char, std::string>>> links;
+
+        // 2 player's 8 of the other player's links' reveal status
+        //   true means revealed to this player
+        vector<vector<bool>> knownLinks;
+
+        /*// 2 player's 16 of all player's links' reveal status
+        // true means revealed to this player
+        // player 1's 8 pieces' status are followed by player 2's
+        vector<vector<bool>> knownLinks;*/
+
+        // 2 player's pair(downloaded data, downloaded virus)
+        vector<std::pair<int, int>> downloadedCounts;
+
+        // remaining ability counters for all players
+        vector<int> abilityRemainingCounts;
+        
+    public:
+        GraphicsDisplay(vector<vector<char>> grid, 
+                    	vector<vector<std::pair<char, std::string>>> links);
+        void notify(Subject &whoFrom);
+        void draw(int currentPlayer);
+};
+#endif
