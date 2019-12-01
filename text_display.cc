ddc541202106bd23f23b2d61bdaba05eaa880e3a
vjsun <victor.jianzhou.sun@edu.uwaterloo.ca>
2019-12-01 16:55:37 -0500

    Simplify printing

--- text_display.cc
+++ text_display.cc
@@ -78,51 +78,25 @@ void TextDisplay::draw(int currentPlayer) {
         << "Abilities: " << abilityRemainingCounts[0] << endl;
 
     if (currentPlayer == 0) {
-        for (int i = 0; i < 3; ++i) {
+        for (int i = 0; i < 8; ++i) {
             cout << links[0][i].first << ": " 
-                << links[0][i].second << "   ";
+                << links[0][i].second;
+            if (i == 3 || i == 7) cout << endl;
+            else cout << "   ";
         }
-        cout << links[0][3].first << ": "
-            << links[0][3].second << endl;
-        for (int i = 4; i < 7; ++i) {
-            cout << links[0][i].first << ": " 
-                << links[0][i].second << "   ";
-        }
-        cout << links[0][7].first << ": "
-            << links[0][7].second << endl;
     }
     else if (currentPlayer == 1) {
-        for (int i = 0; i < 3; ++i) {
+        for (int i = 0; i < 8; ++i) {
             cout << links[0][i].first << ": ";
-            if (knownLinks[1][i]) {   
-                cout << links[0][i].second << "   ";
+            if (knownLinks[1][i]) {
+                cout << links[0][i].second;
+                if (i == 3 || i == 7) cout << endl;
+                else cout << "   ";
+            } else {
+                cout << "?";
+                if (i == 3 || i == 7) cout << endl;
+                else cout << "    ";
             }
-            else {
-                cout << "?    ";
-            }
-        }
-        cout << links[0][3].first << ": ";
-        if (knownLinks[1][3]) {   
-                cout << links[0][3].second << endl;
-            }
-        else {
-            cout << "?" << endl;
-        }
-        for (int i = 4; i < 7; ++i) {
-            cout << links[0][i].first << ": ";
-            if (knownLinks[1][i]) {   
-                cout << links[0][i].second << "   ";
-            }
-            else {
-                cout << "?    ";
-            }
-        }
-        cout << links[0][7].first << ": ";
-        if (knownLinks[1][7]) {   
-                cout << links[0][7].second << endl;
-            }
-        else {
-            cout << "?" << endl;
         }
     }
     else {
@@ -145,52 +119,26 @@ void TextDisplay::draw(int currentPlayer) {
                           << downloadedCounts[1].second << "V" << endl
         << "Abilities: " << abilityRemainingCounts[1] << endl;
 
-    if (currentPlayer == 1) {
-        for (int i = 0; i < 3; ++i) {
-            cout << links[1][i].first << ": " 
-                << links[1][i].second << "   ";
+    if (currentPlayer == 0) {
+        for (int i = 0; i < 8; ++i) {
+            cout << links[1][i].first << ": ";
+            if (knownLinks[0][i]) {
+                cout << links[1][i].second;
+                if (i == 3 || i == 7) cout << endl;
+                else cout << "   ";
+            } else {
+                cout << "?";
+                if (i == 3 || i == 7) cout << endl;
+                else cout << "    ";
+            }
         }
-        cout << links[1][3].first << ": "
-            << links[1][3].second << endl;
-        for (int i = 4; i < 7; ++i) {
-            cout << links[1][i].first << ": " 
-                << links[1][i].second << "   ";
-        }
-        cout << links[1][7].first << ": "
-            << links[1][7].second << endl;
     }
-    else if (currentPlayer == 0) {
-        for (int i = 0; i < 3; ++i) {
-            cout << links[1][i].first << ": ";
-            if (knownLinks[0][i]) {   
-                cout << links[1][i].second << "   ";
-            }
-            else {
-                cout << "?    ";
-            }
-        }
-        cout << links[1][3].first << ": ";
-        if (knownLinks[0][3]) {   
-                cout << links[1][3].second << endl;
-            }
-        else {
-            cout << "?" << endl;
-        }
-        for (int i = 4; i < 7; ++i) {
-            cout << links[1][i].first << ": ";
-            if (knownLinks[0][i]) {   
-                cout << links[1][i].second << "   ";
-            }
-            else {
-                cout << "?    ";
-            }
-        }
-        cout << links[1][7].first << ": ";
-        if (knownLinks[0][7]) {   
-                cout << links[1][7].second << endl;
-            }
-        else {
-            cout << "?" << endl;
+    else if (currentPlayer == 1) {
+        for (int i = 0; i < 8; ++i) {
+            cout << links[1][i].first << ": " 
+                << links[1][i].second;
+            if (i == 3 || i == 7) cout << endl;
+            else cout << "   ";
         }
     }
     else {
