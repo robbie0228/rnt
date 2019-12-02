741a061b30f500b162592178ce6711d0fe870ec7
r67zhao <r67zhao@uwaterloo.ca>
2019-12-02 17:30:06 -0500

    clean up cell

--- cell.cc
+++ cell.cc
@@ -1,96 +1,118 @@
-#include "cell.h"
 #include <cstdlib>
-#include <iostream>
-
+#include "cell.h"
 using namespace std;
 
 void setStateAndNotify(Cell &cell,
-                       int downloadingPlayer, 
-                       char downloadingLinkName, 
-                       LinkType downloadingLinkType, 
-                       bool downloadingLinkIsRevealed, 
-                       bool linkIsRevealed, 
-                       int playerUsingAbility) {
+                       int downloadingPlayer,
+                       char downloadingLinkName,
+                       LinkType downloadingLinkType,
+                       bool downloadingLinkIsRevealed,
+                       bool linkIsRevealed,
+                       int playerUsingAbility)
+{
     cell.setState(StateType{downloadingPlayer,
-                        downloadingLinkName,
-                        downloadingLinkType,
-                        downloadingLinkIsRevealed,
-                        linkIsRevealed,
-                        playerUsingAbility});
+                            downloadingLinkName,
+                            downloadingLinkType,
+                            downloadingLinkIsRevealed,
+                            linkIsRevealed,
+                            playerUsingAbility});
     cell.notifyObservers();
 }
 
-int getPlayerNumFromLink(Link *link) {
+int getPlayerNumFromLink(Link *link)
+{
     return link->getName() < 'a' ? 2 : 1;
 }
 
-Cell::Cell(int row, int col, Link *link, int serverPort):
-    link{link}, row{row}, col{col}, firewall{0}, serverPort{serverPort} {}
+Cell::Cell(int row,
+           int col,
+           Link *link,
+           int serverPort) : link{link},
+                             row{row},
+                             col{col},
+                             firewall{0},
+                             serverPort{serverPort} {}
 
-bool Cell::moveCellHere(Cell &cell) {
+bool Cell::moveCellHere(Cell &cell)
+{
     Link *otherLink = cell.getLink();
-    if (otherLink == nullptr) {
-        throw "Cannot move empty cell, must move your own link";
+    if (otherLink == nullptr)
+    {
+        throw "Cannot move an empty cell, move your own link!";
     }
 
-    int attackPlayer = getPlayerNumFromLink(otherLink);
-    int defensePlayer = (attackPlayer % 2) + 1;
+    int attackerNumber = getPlayerNumFromLink(otherLink);
+    int defenderNumber = (attackerNumber % 2) + 1;
 
-    if (serverPort) {
-        if (serverPort == defensePlayer) {
-            setStateAndNotify(*this,
-                              defensePlayer,
-                              otherLink->getName(),
-                              otherLink->getType(),
-                              true, false, -1);
+    if (serverPort)
+    {
+        if (serverPort == defenderNumber)
+        {
+            setStateAndNotify(*this, defenderNumber, otherLink->getName(),
+                              otherLink->getType(), true, false, -1);
             return true;
-        } else {
-            throw "Invalid move";
         }
-    } else if (firewall) {
-        if (firewall == defensePlayer) {
+        else
+        {
+            throw "Cannot move onto your own server port!";
+        }
+    }
+    else if (firewall)
+    {
+        if (firewall == defenderNumber)
+        {
             link = otherLink;
             setStateAndNotify(*this, -1, '.', LinkType::NoType,
                               false, true, -1);
-            if (otherLink->getType() == LinkType::Virus) {
+            if (otherLink->getType() == LinkType::Virus)
+            {
                 link = nullptr;
-                setStateAndNotify(*this,
-                                  attackPlayer,
-                                  otherLink->getName(),
-                                  otherLink->getType(),
-                                  false, false, -1);
+                setStateAndNotify(*this, attackerNumber, otherLink->getName(),
+                                  otherLink->getType(), false, false, -1);
                 return true;
-            } else {
+            }
+            else
+            {
                 return false;
             }
-        } else {
+        }
+        else
+        {
             link = otherLink;
             setStateAndNotify(*this, -1, '.', LinkType::NoType,
-                          false, false, -1);
+                              false, false, -1);
             return false;
         }
-    } else if (link == nullptr) {
+    }
+    else if (link == nullptr)
+    {
         link = cell.getLink();
         setStateAndNotify(*this, -1, '.', LinkType::NoType,
                           false, false, -1);
         return false;
-    } else {
-        if (abs(otherLink->getName() - link->getName()) < NUMLINKS) {
-            throw "Cannot move a link onto another of your links";
+    }
+    else
+    {
+        if (abs(otherLink->getName() - link->getName()) < NUMLINKS)
+        {
+            throw "Cannot move a link onto another of your links!";
         }
-        if (otherLink->getStrength() >= link->getStrength()) {
+        if (otherLink->getStrength() >= link->getStrength())
+        {
             char downloadLinkName = link->getName();
             LinkType downloadLinkType = link->getType();
             link = otherLink;
             setStateAndNotify(*this,
-                              attackPlayer,
+                              attackerNumber,
                               downloadLinkName,
                               downloadLinkType,
                               true, true, -1);
             return false;
-        } else {
+        }
+        else
+        {
             setStateAndNotify(*this,
-                              defensePlayer,
+                              defenderNumber,
                               otherLink->getName(),
                               otherLink->getType(),
                               true, true, -1);
@@ -99,90 +121,92 @@ bool Cell::moveCellHere(Cell &cell) {
     }
 }
 
-char Cell::getName() const{
-    if (link != nullptr) {
+char Cell::getName() const
+{
+    if (link != nullptr)
         return link->getName();
-    } else if (serverPort != 0) {
+    else if (serverPort != 0)
         return 'S';
-    } else if (firewall != 0) {
+    else if (firewall != 0)
         return firewall == 1 ? 'm' : 'w';
-    } else {
+    else
         return '.';
-    }
 }
 
-Link *Cell::getLink() const{
+Link *Cell::getLink() const
+{
     return link;
 }
 
-void Cell::useAbility(Ability a, int user) {
-    switch (a) {
-        case Ability::Boost :
+void Cell::useAbility(Ability abilityName, int user)
+{
+    switch (abilityName)
+    {
+    case Ability::Boost:
+    {
+        int currSpeed = this->link->getSpeed();
+        this->link->setSpeed(1 + currSpeed);
+        setStateAndNotify(*this, -1, '.', LinkType::NoType, false, false, user);
+        break;
+    }
+    case Ability::Polarize:
+    {
+        LinkType currType = this->link->getType();
+        if (currType == LinkType::Data)
         {
-            int currSpeed = this->link->getSpeed();
-            this->link->setSpeed(1 + currSpeed);
-            setStateAndNotify(*this, -1, '.', LinkType::NoType, false,
-                              false, user);
-            break;
+            this->link->setType(LinkType::Virus);
         }
-        case Ability::Polarize :
+        else
         {
-            LinkType currType = this->link->getType();
-            if (currType == LinkType::Data) {
-                this->link->setType(LinkType::Virus);
-            } else {
-                this->link->setType(LinkType::Data);
-            }
-            setStateAndNotify(*this, -1, '.', LinkType::NoType, false,
-                              false, user);
-            break;
+            this->link->setType(LinkType::Data);
         }
-        case Ability::Download :
-        {
-            this->removeAndDownload(user, user);
-            break;
-        }
-        case Ability::Firewall : 
-        {
-            this->firewall = user;
-            setStateAndNotify(*this, -1, '.', LinkType::NoType, false,
-                              false, user);
-            break;
-        }
-        case Ability::Scan :
-            setStateAndNotify(*this, -1, '.', LinkType::NoType, false,
-                              true, user);
-            break;
-        default :
-        {
-            setStateAndNotify(*this, -1, '.', LinkType::NoType, false,
-                              false, user);
-            break;
-        }
+        setStateAndNotify(*this, -1, '.', LinkType::NoType, false, false, user);
+        break;
+    }
+    case Ability::Download:
+    {
+        this->removeAndDownload(user, user);
+        break;
+    }
+    case Ability::Firewall:
+    {
+        this->firewall = user;
+        setStateAndNotify(*this, -1, '.', LinkType::NoType, false, false, user);
+        break;
+    }
+    case Ability::Scan:
+        setStateAndNotify(*this, -1, '.', LinkType::NoType, false, true, user);
+        break;
+    default:
+    {
+        setStateAndNotify(*this, -1, '.', LinkType::NoType, false, false, user);
+        break;
+    }
     }
 }
 
-void Cell::removeLink() {
+void Cell::removeLink()
+{
     link = nullptr;
-    setStateAndNotify(*this, -1, '.', LinkType::NoType,
-                      false, false, -1);
+    setStateAndNotify(*this, -1, '.', LinkType::NoType, false, false, -1);
 }
 
-void Cell::removeAndDownload(int downloadingPlayer, int playerUsingAbility) {
+void Cell::removeAndDownload(int downloadingPlayer, int playerUsingAbility)
+{
     char linkName = link->getName();
     LinkType linkType = link->getType();
     link = nullptr;
-    setStateAndNotify(*this, downloadingPlayer,
-                      linkName,
-                      linkType,
+    setStateAndNotify(*this, downloadingPlayer, linkName, linkType,
                       false, false, playerUsingAbility);
 }
 
-void Cell::setLink(Link *newLink) {
+void Cell::setLink(Link *newLink)
+{
     link = newLink;
 }
 
-InfoType Cell::getInfo() const{
+InfoType Cell::getInfo() const
+{
     return {row, col, getName(),
             (link == nullptr ? LinkType::NoType : link->getType()),
             (link == nullptr ? -1 : link->getStrength())};
--- cell.h
+++ cell.h
@@ -1,37 +1,42 @@
 #ifndef CELL_H
 #define CELL_H
+
 #include "link.h"
 #include "subject.h"
 
-class Cell : public Subject {
+class Cell : public Subject
+{
         Link *link; // If Cell contains a Link: link points to that Link;
                     //               otherwise: link is a nullptr
-        int row;    // 1 <= row <= 8
-        int col;    // 1 <= col <= 8
-        int firewall;   // If Cell contains a firewall owned by Player n: 
-                        //  firewall = n; 
-                        //                                     otherwise:
-                        //  firewall = 0;
-        int serverPort; // If Cell contains a server port owned by Player n: 
+
+        int row; // 1 <= row <= 8
+        int col; // 1 <= col <= 8
+
+        int firewall; // If Cell contains a firewall owned by Player n:
+                      //  firewall = n;
+                      //                                     otherwise:
+                      //  firewall = 0;
+
+        int serverPort; // If Cell contains a server port owned by Player n:
                         //  serverPort = n;
                         //                                        otherwise:
                         //  serverPort = 0;
 
-    public:
+public:
         // Constructor for Cell, default to an empty Cell
         Cell(int row, int col, Link *link = nullptr, int serverPort = 0);
 
-        // Moves a Link from the given cell to this Cell
+        // Moves a Link from cell to this Cell
         //   If this Cell has a Link, battle is taken care of
         //   If this Cell has a firewall, consequences are taken care of
         //   If this Cell has a server port, consequences are taken care of
         // Returns true if link stayed the same, otherwise false.
-        bool moveCellHere(Cell &cell); 
+        bool moveCellHere(Cell &cell);
 
         // Returns this Cell's name
         char getName() const;
 
-        // Returns this Cell's link (pointer)
+        // Returns a pointer to this Cell's link
         Link *getLink() const;
 
         // Uses the specified ability on this cell
@@ -49,6 +54,7 @@ class Cell : public Subject {
         // Set this Cell's link (pointer) to newLink
         void setLink(Link *newLink);
 
+        // Returns a set of information about this Cell contained in an InfoType
         InfoType getInfo() const;
 };
 
