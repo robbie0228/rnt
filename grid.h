21c0a47e7808bd772c0be7ec3da5041c61ad8cd7
r67zhao <r67zhao@uwaterloo.ca>
2019-12-02 18:18:21 -0500

    clean up grid

--- grid.h
+++ grid.h
@@ -1,5 +1,6 @@
 #ifndef GRID_H
 #define GRID_H
+
 #include <vector>
 #include <utility>
 #include <iostream>
@@ -15,36 +16,40 @@ class Cell;
 class Link;
 class Player;
 
-class Grid {
-        // 8*8 2D array of Cell's
-        vector<vector<Cell>> cells;
+class Grid
+{
+    // 8*8 2D array of Cell's
+    vector<vector<Cell>> cells;
 
-        // Every Player's each of the 8 Links have a row-col coordianate pair.
-        //   If a Link perishes its coordianate pair is set to <-1, -1>
-        vector<vector<std::pair<int, int>>> locationOfLinks;
+    // Every Player's each of the 8 Links have a row-col coordianate pair.
+    //   If a Link perishes its coordianate pair is set to <-1, -1>
+    vector<vector<std::pair<int, int>>> locationOfLinks;
 
-        std::unique_ptr<TextDisplay> textDisplay;
+    // A pointer to the text-style display
+    std::unique_ptr<TextDisplay> textDisplay;
 
-        std::unique_ptr<GraphicsDisplay> graphicsDisplay;
+    // A pointer to the graphic-style display
+    std::unique_ptr<GraphicsDisplay> graphicsDisplay;
 
-        bool useGraphics; // Whether or not to use graphics
+    // An interpretation from the command line argument
+    //  true means graphic-style display is enabled
+    bool useGraphics;
 
-    public:
-        // Constructor for a Grid
-        //   Instantiates Cells in a size*size Grid
-        Grid(vector<Player *> players, 
-             vector<vector<Link *>> linkPointers, 
-             bool useGraphics);
+public:
+    // Constructor for a Grid
+    //   Instantiates Cells in a size*size Grid
+    Grid(vector<Player *> players,
+         vector<vector<Link *>> linkPointers,
+         bool useGraphics);
 
-        // Moves Player (player + 1)'s Link (link + 1), towards Direction dir
-        void move(int player, int link, Direction dir);
+    // Moves Player (player + 1)'s Link (link + 1), towards Direction dir
+    void move(int player, int link, Direction dir);
 
-        // Uses ability
-        void useAbility(Ability a, vector<char> v, int user);
+    // Uses ability
+    void useAbility(Ability a, vector<char> v, int user);
 
-        // Calls any type of Display to draw
-        // friend std::ostream &operator<<(std::ostream &out, const Grid &grid);
-        void printBoard(int currentPlayer);
+    // Calls any type of Display to draw
+    void printBoard(int currentPlayer);
 };
 
 #endif
