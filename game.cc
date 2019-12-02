21d82a8baf59577aedb7172a04c68b62461a9a83
r67zhao <r67zhao@uwaterloo.ca>
2019-12-02 17:58:02 -0500

    clean up game

--- game.cc
+++ game.cc
@@ -1,29 +1,30 @@
 #include "game.h"
 using namespace std;
 
-// helper functions
-
-int charLinkToInt(char c) {
-    if ('a' <= c && c <= 'h') {
+// This helper function converts bewteen a Link's name and corresponding index
+int charLinkToInt(char c)
+{
+    if ('a' <= c && c <= 'h')
         return c - 'a';
-    } else if ('A' <= c && c <= 'H') {
+    else if ('A' <= c && c <= 'H')
         return c - 'A';
-    }
-    throw "Invalid Link";
+    else
+        throw "Invalid Link";
 }
 
-//implementations
-
 Game::Game(vector<pair<int, vector<pair<Ability, bool>>>> cmdAllAbilities,
-           vector<pair<int, vector<Link>>> cmdAllLinks, bool useGraphics): 
-                currentPlayer{0}, abilityUsedCount{0} {
+           vector<pair<int, vector<Link>>> cmdAllLinks, bool useGraphics)
+    : currentPlayer{0}, abilityUsedCount{0}
+{
 
     Player p1 = Player(1);
     Player p2 = Player(2);
 
-    if (cmdAllAbilities.size()){
-        for (int i = 0; i < cmdAllAbilities.size(); ++i) {
-            if (cmdAllAbilities[i].first == 1) 
+    if (cmdAllAbilities.size())
+    {
+        for (int i = 0; i < cmdAllAbilities.size(); ++i)
+        {
+            if (cmdAllAbilities[i].first == 1)
                 p1.overrideAbilities(cmdAllAbilities[i].second);
             else if (cmdAllAbilities[i].first == 2)
                 p2.overrideAbilities(cmdAllAbilities[i].second);
@@ -34,81 +35,121 @@ Game::Game(vector<pair<int, vector<pair<Ability, bool>>>> cmdAllAbilities,
     this->players.push_back(p2);
 
     vector<Player *> playerPointers;
-    for (int i = 0; i < NUMPLAYERS; ++i) {
+    for (int i = 0; i < NUMPLAYERS; ++i)
+    {
         playerPointers.emplace_back(&(players[i]));
     }
 
     vector<vector<Link *>> allLinks;
-    for (int i = 0; i < NUMPLAYERS; ++i) {
+    for (int i = 0; i < NUMPLAYERS; ++i)
+    {
         int playerNumber = i + 1;
         int cmdSchemeFoundAt = -1;
-        if (cmdAllLinks.size()) {
-            for (int j = 0; j < cmdAllLinks.size(); ++j) {
-                if (cmdAllLinks[j].first == playerNumber) {
+        if (cmdAllLinks.size())
+        {
+            for (int j = 0; j < cmdAllLinks.size(); ++j)
+            {
+                if (cmdAllLinks[j].first == playerNumber)
+                {
                     cmdSchemeFoundAt = j;
                     break;
                 }
             }
         }
-        if (cmdSchemeFoundAt == -1) allLinks.emplace_back(players[i].init());
-        else allLinks.emplace_back(players[i].cmdInit(cmdAllLinks[cmdSchemeFoundAt].second));
+        if (cmdSchemeFoundAt == -1)
+            allLinks.emplace_back(players[i].init());
+        else
+            allLinks.emplace_back(players[i]
+                                      .cmdInit(
+                                          cmdAllLinks[cmdSchemeFoundAt]
+                                              .second));
     }
 
     grid = make_unique<Grid>(playerPointers, allLinks, useGraphics);
 }
 
-void Game::move(char link, Direction dir) {
-    if ((link < 'a' && currentPlayer == 0) || 
-        (link >= 'a' && currentPlayer == 1)) {
-            throw "Cannot move opponent's piece";
+void Game::move(char link, Direction dir)
+{
+    if ((link < 'a' && currentPlayer == 0) ||
+        (link >= 'a' && currentPlayer == 1))
+    {
+        throw "Cannot move opponent's piece!";
     }
+
     this->grid->move(currentPlayer, charLinkToInt(link), dir);
-    if (currentPlayer == 0) {
+
+    if (currentPlayer == 0)
+    {
         currentPlayer = 1;
-    } else if (currentPlayer == 1) {
+    }
+    else if (currentPlayer == 1)
+    {
         currentPlayer = 0;
     }
+
     abilityUsedCount = 0;
 }
 
-void Game::printBoard() {
+void Game::printBoard()
+{
     grid->printBoard(currentPlayer);
 }
 
-void Game::printAbilities(ostream& out){
+void Game::printAbilities(ostream &out)
+{
     players[currentPlayer].printAbilities(out);
 }
 
-pair<int, bool> Game::verifyAbility(int abilityID) {
-    pair<Ability, bool> abilityPair = players[currentPlayer].getAbility(abilityID);
-    if (abilityPair.first == Ability::Firewall) {
+pair<int, bool> Game::verifyAbility(int abilityID)
+{
+    pair<Ability, bool> abilityPair = players[currentPlayer]
+                                          .getAbility(abilityID);
+
+    if (abilityPair.first == Ability::Firewall)
+    {
         return make_pair(2, abilityPair.second);
-    } else if (abilityPair.first == Ability::Download || abilityPair.first == Ability::Boost || 
-               abilityPair.first == Ability::Scan || abilityPair.first == Ability::Polarize || 
-               abilityPair.first == Ability::Uber) {
+    }
+    else if (abilityPair.first == Ability::Download ||
+             abilityPair.first == Ability::Boost ||
+             abilityPair.first == Ability::Scan ||
+             abilityPair.first == Ability::Polarize ||
+             abilityPair.first == Ability::Uber)
+    {
         return make_pair(1, abilityPair.second);
     }
     return make_pair(0, false);
 }
 
-void Game::useAbility(int abilityID, vector<char> useAbilityInfo) {
-    if (abilityUsedCount >= MAXABILITYUSE) throw "Only use one ability per turn";
-    else {
-        Ability abilityName = 
+void Game::useAbility(int abilityID, vector<char> useAbilityInfo)
+{
+    if (abilityUsedCount >= MAXABILITYUSE)
+    {
+        throw "Only use one ability per turn!";
+    }
+    else
+    {
+        Ability abilityName =
             players[currentPlayer].useAbility(abilityID, useAbilityInfo);
+
         grid->useAbility(abilityName, useAbilityInfo, currentPlayer + 1);
         abilityUsedCount += 1;
     }
 }
 
-int Game::checkStatus() {
+int Game::checkStatus()
+{
     int curStatus = -1;
-    for (int i = 0; i < NUMPLAYERS; i++) {
-        if (players[i].checkStatus() == PlayStatus::Win) {
+    for (int i = 0; i < NUMPLAYERS; i++)
+    {
+        if (players[i].checkStatus() == PlayStatus::Win)
+        {
             curStatus = i + 1;
-        } else if (players[i].checkStatus() == PlayStatus::Lose) {
+        }
+        else if (players[i].checkStatus() == PlayStatus::Lose)
+        {
             curStatus = (i + 1) % NUMPLAYERS + 1;
         }
     }
+    
     return curStatus;
 }
--- game.h
+++ game.h
@@ -1,5 +1,6 @@
 #ifndef GAME_H
 #define GAME_H
+
 #include <vector>
 #include <iostream>
 #include <memory>
@@ -9,43 +10,44 @@
 using std::pair;
 using std::vector;
 
-class Game {
-        // grid is the Grid that the game takes place in
-        std::unique_ptr<Grid> grid;
-        // a vector of two Player's
-        vector<Player> players;
-        // an integer that holds info on the current Player
-        int currentPlayer;
-        // the number of abilities the current player has used in the turn
-        int abilityUsedCount;
+class Game
+{
+    // grid is the Grid that the game takes place in
+    std::unique_ptr<Grid> grid;
+    // a vector of two Player's
+    vector<Player> players;
+    // an integer that holds info on the current Player
+    int currentPlayer;
+    // the number of abilities the current player has used in the turn
+    int abilityUsedCount;
 
-    public:
-        // Constructor for a Game
-        //   Instantiates a grid and a vector of players
-        //   Uses link information from each player to instantiate grid
-        Game(vector<pair<int, vector<pair<Ability, bool>>>> cmdAllAbilities,
-             vector<pair<int, vector<Link>>> cmdAllLinks, bool useGraphics);
+public:
+    // Constructor for a Game
+    //   Instantiates a grid and a vector of players
+    //   Uses link information from each player to instantiate grid
+    Game(vector<pair<int, vector<pair<Ability, bool>>>> cmdAllAbilities,
+         vector<pair<int, vector<Link>>> cmdAllLinks, bool useGraphics);
 
-        // Moves Link-link towards Direction dir;
-        //   Parses link and calls Grid's move() with parsed information
-        void move(char link, Direction dir);
+    // Moves Link-link towards Direction dir;
+    //   Parses link and calls Grid's move() with parsed information
+    void move(char link, Direction dir);
 
-        // Verifies if the player has the specified ability, and returns
-        //   how many chars of information the ability requires
-        pair<int, bool> verifyAbility(int abilityID);
+    // Verifies if the player has the specified ability, and returns
+    //   how many chars of information the ability requires
+    pair<int, bool> verifyAbility(int abilityID);
 
-        // Player consumes an ability
-        void useAbility(int abilityID, vector<char> useAbilityInfo);
+    // Player consumes an ability
+    void useAbility(int abilityID, vector<char> useAbilityInfo);
 
-        // Calls current player's printAbilities()
-        void printAbilities(std::ostream& out);
+    // Calls current player's printAbilities()
+    void printAbilities(std::ostream &out);
 
-        // Prints all information about the game at that current time
-        void printBoard();
+    // Prints all information about the game at that current time
+    void printBoard();
 
-        // Checks the status of all players to see if any won/lost
-        //   Returns the player that won or -1 if still in play
-        int checkStatus();
+    // Checks the status of all players to see if anyone wins or loses
+    //   Returns the player that wins or -1 if still in play
+    int checkStatus();
 };
 
 #endif
