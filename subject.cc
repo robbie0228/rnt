9ae1bf7ec93d0957e2d68706a4f678e7a50863eb
r67zhao <r67zhao@uwaterloo.ca>
2019-12-02 19:00:06 -0500

    clean up subject

--- subject.cc
+++ subject.cc
@@ -1,13 +1,22 @@
 #include "subject.h"
 
-void Subject::attach(Observer *o) {
+void Subject::attach(Observer *o)
+{
   observers.emplace_back(o);
 }
 
-void Subject::notifyObservers() {
-  for (auto &ob : observers) ob->notify(*this);
+void Subject::notifyObservers()
+{
+  for (auto &ob : observers)
+    ob->notify(*this);
 }
 
-void Subject::setState(StateType newS) { state = newS; }
+void Subject::setState(StateType newS)
+{
+  state = newS;
+}
 
-StateType Subject::getState() const { return state; }
+StateType Subject::getState() const
+{
+  return state;
+}
--- subject.h
+++ subject.h
@@ -1,16 +1,17 @@
 #ifndef SUBJECT_H
 #define SUBJECT_H
+
 #include <vector>
 #include <map>
 #include <string>
 #include "enums.h"
 #include "observer.h"
-
-using std::string;
 using std::map;
+using std::string;
 using std::vector;
 
-struct StateType {
+struct StateType
+{
 	// Which player is downloading a link, if any
 	//   -1 if no player
 	int downloadingPlayer;
@@ -34,7 +35,9 @@ struct StateType {
 	int playerUsingAbility;
 };
 
-struct InfoType {
+// For aggregation of information on a Link
+struct InfoType
+{
 	int row;
 	int col;
 	char name;
@@ -42,16 +45,21 @@ struct InfoType {
 	int strength;
 };
 
-class Subject {
-		vector<Observer *> observers;
-		StateType state;
-	public:
-		void attach(Observer *o);  
-		void notifyObservers();
-		void setState(StateType newS);
-		StateType getState() const;
-		virtual InfoType getInfo() const = 0;
-		virtual ~Subject() {};
+class Subject
+{
+	// Vector of pointers to Observer's
+	vector<Observer *> observers;
+	// An aggregation of information
+	StateType state;
+
+public:
+	// Self-explainatory Subject methods
+	void attach(Observer *o);
+	void notifyObservers();
+	void setState(StateType newS);
+	StateType getState() const;
+	virtual InfoType getInfo() const = 0;
+	virtual ~Subject(){};
 };
 
 #endif
